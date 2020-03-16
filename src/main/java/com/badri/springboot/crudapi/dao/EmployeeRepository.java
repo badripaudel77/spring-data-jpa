97656d6264954724732fd3d24016bcd05768581f
badrivai <badripaudel77@gmail.com>
2020-03-16 19:53:47 +0545

    adding .......

--- src/main/java/com/badri/springboot/crudapi/dao/EmployeeRepository.java
+++ src/main/java/com/badri/springboot/crudapi/dao/EmployeeRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.badri.springboot.crudapi.entity.Employee;
 
+//base path in .properties file. /data-rest-api
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 	//no code to write , as we got a lot of methods for free.
